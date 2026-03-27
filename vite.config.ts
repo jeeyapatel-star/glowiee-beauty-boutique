081fddbb9a2b953eb74cfecb15884362acc3d8c7
jeeyapatel-star <jeeya.patel@srvmedia.com>
2026-03-27 11:26:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/glowiee-beauty-boutique/',
+})
+  import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
